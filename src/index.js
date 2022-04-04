794f8b28ad3ac3831a0e1bdc459d8e1f2ef56971
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-04-05 00:38:40 +0900

    schema

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("apollo-server");
 
+// 블로그 > 카테고리,게시글,댓글,유저,좋아요,이미지
 const typeDefs = `
   type Query{
     hello:String!
@@ -20,11 +21,11 @@ const resolvers = {
       return "pong";
     },
     user() {
-      return "kku"; // 페어런트 밑에 보내줌 props 로
+      return "kku"; // 페어런트 밑에 보내줌 props 로 타고밑으로 감
     },
   },
   User: {
-    name(parent) {
+    name() {
       return "ezdar";
     },
   },
